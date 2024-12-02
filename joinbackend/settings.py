a2f8c729ac1101e9152474c647f6571d067ac11a
Lukas Nolting <lukas.nolting@icloud.com>
2024-12-02 16:51:04 +0000

    static

--- joinbackend/settings.py
+++ joinbackend/settings.py
@@ -133,11 +133,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+STATIC_URL = 'join/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+
+MEDIA_URL = '/join/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # Default primary key field type
